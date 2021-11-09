d8cc4bd64bef6e974b8dbbccfea63464e366bc1b
Raihan Rasheed Apurbo <apurbo97@gmail.com>
2021-11-09 09:14:10 +0600

    bug fix on video id doesn't exist

--- src/videoapi/cli.py
+++ src/videoapi/cli.py
@@ -1,24 +1,35 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 
 videos = {}
 
 video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("name", type=str, help="Name of the video is required", required = True)
-video_put_args.add_argument("views", type=str, help="Views of the video is required", required = True)
-video_put_args.add_argument("likes", type=str, help="Likes on the video is required", required = True)
+video_put_args.add_argument(
+    "name", type=str, help="Name of the video is required", required=True
+)
+video_put_args.add_argument(
+    "views", type=str, help="Views of the video is required", required=True
+)
+video_put_args.add_argument(
+    "likes", type=str, help="Likes on the video is required", required=True
+)
+
+
+def abort_if_video_id_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort("video id is not valid...")
 
 
 class Video(Resource):
-    
     def get(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
         return videos[video_id]
 
     def put(self, video_id):
         args = video_put_args.parse_args()
         videos[video_id] = args
         return videos[video_id], 201
-    
+
 
 def main():
 
